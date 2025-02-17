66f85f56962dcf938dbe217a00899f95b26e950a
cougarbellinger <cougarbellinger@arizona.edu>
2025-02-16 21:21:00 -0700

    fixed errors in assignment 4.

--- Assignments/Assignment4/my-app/src/main/java/assignment4/app/MyListDemo.java
+++ Assignments/Assignment4/my-app/src/main/java/assignment4/app/MyListDemo.java
@@ -7,20 +7,82 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 public class MyListDemo extends URLLoader {
-    protected List<String> list = new ArrayList<>();
+    protected List<Product> list = new ArrayList<>();
 
     @Override
     protected void processLine(String tokens) {
-        list.add(tokens);
+    }
+
+    @Override
+    protected void processLine(String[] tokens) {
+        if (tokens[7].equalsIgnoreCase("British Columbia")) {
+            Product product = new Product();
+            product.setId(Long.parseLong(tokens[0]));
+            product.setName(tokens[1]);
+            product.setAgentName(tokens[2]);
+            product.setAgentId(Long.parseLong(tokens[3]));
+            product.setPrice(Double.parseDouble(tokens[5]));
+            product.setTerritory(tokens[7]);
+            product.setCategory(tokens[8]);
+
+            list.add(product);
+        }
+    }
+
+    private void createList(Product product, Row row) {
+        Cell cell = row.createCell(0);
+        cell.setCellValue(product.getId());
+
+        cell = row.createCell(1);
+        cell.setCellValue(product.getName());
+
+        cell = row.createCell(2);
+        cell.setCellValue(product.getAgentName());
+
+        cell = row.createCell(3);
+        cell.setCellValue(product.getAgentId());
+
+        cell = row.createCell(4);
+        cell.setCellValue(product.getTerritory());
+
+        cell = row.createCell(5);
+        cell.setCellValue(product.getCategory());
+    }
+
+    protected void createXLS() {
+        try {
+            XSSFWorkbook workbook = new XSSFWorkbook();
+            XSSFSheet sheet = workbook.createSheet("sheet1");// creating a blank sheet
+            int rownum = 0;
+            for (Product product : list) {
+                Row row = sheet.createRow(rownum++);
+                createList(product, row);
+            }
+            FileOutputStream out = new FileOutputStream(new File("NewFile3.xlsx")); // file name with path
+            workbook.write(out);
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
         MyListDemo myListDemo = new MyListDemo();
         myListDemo.loadData();
         System.out.println(myListDemo.list.size());
+
+        myListDemo.createXLS();
     }
 }
--- Assignments/Assignment4/my-app/src/main/java/assignment4/app/URLLoader.java
+++ Assignments/Assignment4/my-app/src/main/java/assignment4/app/URLLoader.java
@@ -1,11 +1,13 @@
 package assignment4.app;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 public abstract class URLLoader {
     protected abstract void processLine(String tokens);
@@ -13,29 +15,22 @@ public abstract class URLLoader {
     protected abstract void processLine(String[] tokens);
 
     public final void loadData() {
-        URL url = null;
-        BufferedReader in = null;
+        File in = new File("Sample-Spreadsheet-1000-rows.csv");
         try {
-            url = new URL("https://sample-videos.com/csv/Sample-Spreadsheet-1000-rows.csv");
-            // this works on Cerny's try UTF-8 if messy chars
-            in = new BufferedReader(new InputStreamReader(url.openStream(),
-                    StandardCharsets.ISO_8859_1));
             String inputLine;
-            while ((inputLine = in.readLine()) != null) {
-                System.out.println(inputLine);
+            String[] line;
+            Scanner reader = new Scanner(in);
+            while (reader.hasNextLine()) {
+                inputLine = reader.nextLine();
+                line = inputLine.split(",");
+                processLine(line);
             }
-        } catch (MalformedURLException e2) {
-            e2.printStackTrace();
+
+            if (in != null) {
+                reader.close();
+            }
         } catch (IOException e1) {
             e1.printStackTrace();
-        } finally {
-            try {
-                if (in != null) {
-                    in.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
 
