6ca335fd611d86ec3bd195372197f515fa6eb847
CougarBellinger <cougarbellinger@gmail.com>
2025-03-23 22:13:54 -0700

    changed to user input

--- Assignments/Assignment8/assignment8/src/main/java/com/assignment8/IceCreamServer.java
+++ Assignments/Assignment8/assignment8/src/main/java/com/assignment8/IceCreamServer.java
@@ -9,12 +9,15 @@ import java.net.Socket;
 
 public class IceCreamServer {
     public static void main(String[] args) throws IOException {
+        int portNumber;
         if (args.length != 1) {
-            System.err.println("Usage: java IceCreamServer <port number>");
-            System.exit(1);
+            System.err.println("Usage: java IceCreamServer <port number>\nUsing defaults \"localhost\" and \"2222\"");
+            portNumber = 2222;
+            // System.exit(1);
+        } else {
+            portNumber = Integer.parseInt(args[0]);
         }
 
-        int portNumber = Integer.parseInt(args[0]);
         try (
                 ServerSocket serverSocket = new ServerSocket(portNumber);
                 Socket clientSocket = serverSocket.accept();
